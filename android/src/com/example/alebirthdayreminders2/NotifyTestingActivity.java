f5ccbfc13cd4367a38bd749d8727fb6c34b01129
Amnon Aaronsohn <amnonaar@localhost>
2014-05-01 10:48:02 +0300

    Add NotifyTesting activity.

---
+++ android/src/com/example/alebirthdayreminders2/NotifyTestingActivity.java
@@ -0,0 +1,22 @@
+package com.example.alebirthdayreminders2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NotifyTestingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_notify_testing);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.notify_testing, menu);
+		return true;
+	}
+
+}
